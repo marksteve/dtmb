cfce44850d2db0c174ae5655b3601d2a0706c08a
Austin Hallock <austinhallock@gmail.com>
2014-02-14 05:22:39 +0100

    Clay and Kik integration + name change

--- main.js
+++ main.js
@@ -20,6 +20,18 @@ WebFontConfig = {
     s.parentNode.insertBefore(wf, s);
 })(); 
 
+// Load in Clay.io API
+var Clay = Clay || {};
+Clay.gameKey = "dtmb";
+Clay.readyFunctions = [];
+Clay.ready = function( fn ) {
+    Clay.readyFunctions.push( fn );
+};
+( function() {
+    var clay = document.createElement("script"); clay.async = true;
+    clay.src = "http://cdn.clay.io/api.js"; 
+    var tag = document.getElementsByTagName("script")[0]; tag.parentNode.insertBefore(clay, tag);
+} )();
 
 function main() {
 
@@ -42,6 +54,57 @@ var game = new Phaser.Game(
     false
 );
 
+
+function clayLoaded() {
+    // Set up the menu items
+    var options = {
+        items: [
+            { title: 'View High Scores', handler: showScores }
+        ]
+    };
+    Clay.UI.Menu.init(options);
+    
+    leaderboard = new Clay.Leaderboard({ id: 2797 });
+}
+Clay.ready(clayLoaded);
+
+function showScores() {
+    if (leaderboard) {
+        leaderboard.show({ best: true });
+    }
+}
+function kikThis() {
+    Clay.Kik.post( { message: 'I just scored ' + score + ' in Snappy Bird! Think you can beat my score?', title: 'Snappy Bird!' } );
+}
+function postScore() {
+    if( postingScore ) // skip if it's already trying to post the score...
+        return;
+    postScoreText.setText('...');
+    postingScore = true;
+    
+    var post = function() {
+    	if(!leaderboard) return;
+        leaderboard.post({ score: score }, function() {
+            showScores();
+            postScoreText.setText('POST\nSCORE!');
+            postingScore = false;
+        });
+    }
+    
+    if (Clay.Environment.platform == 'kik') {
+	    Clay.Kik.connect({}, function(response) {
+	        if (response.success) {
+	            Clay.Player.onUserReady( post );
+	        } else {
+	            postScoreText.setText('POST\nSCORE!');            
+	            postingScore = false;
+	        }
+	    });
+    } else {
+    	post();
+    }
+    	
+}
 function preload() {
     var assets = {
         spritesheet: {
@@ -78,6 +141,11 @@ var gameStarted,
     scoreText,
     instText,
     gameOverText,
+    kikThisText,
+    kikThisClickArea,
+    postScoreClickArea,
+    postingScore,
+    leaderboard,
     flapSnd,
     scoreSnd,
     hurtSnd,
@@ -126,13 +194,13 @@ function create() {
     // Add score text
     scoreText = game.add.text(
         game.world.width / 2,
-        game.world.height / 4,
+        game.world.height / 8,
         "",
         {
-            font: '16px "Press Start 2P"',
+            font: '32px "Press Start 2P"',
             fill: '#fff',
             stroke: '#430',
-            strokeThickness: 4,
+            strokeThickness: 8,
             align: 'center'
         }
     );
@@ -143,10 +211,10 @@ function create() {
         game.world.height - game.world.height / 4,
         "",
         {
-            font: '8px "Press Start 2P"',
+            font: '16px "Press Start 2P"',
             fill: '#fff',
             stroke: '#430',
-            strokeThickness: 4,
+            strokeThickness: 8,
             align: 'center'
         }
     );
@@ -154,18 +222,61 @@ function create() {
     // Add game over text
     gameOverText = game.add.text(
         game.world.width / 2,
-        game.world.height / 2,
+        game.world.height / 4,
         "",
         {
-            font: '16px "Press Start 2P"',
+            font: '32px "Press Start 2P"',
             fill: '#fff',
             stroke: '#430',
-            strokeThickness: 4,
+            strokeThickness: 8,
             align: 'center'
         }
     );
     gameOverText.anchor.setTo(0.5, 0.5);
-    gameOverText.scale.setTo(2, 2);
+    //gameOverText.scale.setTo(2, 2);
+    
+    
+    // Add kik this text (hidden until game is over)
+    postScoreText = game.add.text(
+        game.world.width / 4,
+        game.world.height / 2,
+        "",
+        {
+            font: '20px "Press Start 2P"',
+            fill: '#fff',
+            stroke: '#430',
+            strokeThickness: 8,
+            align: 'center'
+        }
+    );
+    postScoreText.setText("POST\nSCORE!");
+    postScoreText.anchor.setTo(0.5, 0.5);
+    postScoreText.renderable = false;
+    //postScoreText.scale.setTo(2, 2);
+    // So we can have clickable text... we check if the mousedown/touch event is within this rectangle inside flap()
+    postScoreClickArea = new Phaser.Rectangle(postScoreText.x - postScoreText.width / 2, postScoreText.y - postScoreText.height / 2, postScoreText.width, postScoreText.height);
+    
+    // Add kik this text (hidden until game is over)
+    kikThisText = game.add.text(
+        3 * game.world.width / 4,
+        game.world.height / 2,
+        "",
+        {
+            font: '20px "Press Start 2P"',
+            fill: '#fff',
+            stroke: '#430',
+            strokeThickness: 8,
+            align: 'center'
+        }
+    );
+    kikThisText.setText("KIK\nTHIS!");
+    kikThisText.anchor.setTo(0.5, 0.5);
+    kikThisText.renderable = false;
+    //kikThisText.scale.setTo(2, 2);
+    // So we can have clickable text... we check if the mousedown/touch event is within this rectangle inside flap()
+    kikThisClickArea = new Phaser.Rectangle(kikThisText.x - kikThisText.width / 2, kikThisText.y - kikThisText.height / 2, kikThisText.width, kikThisText.height);
+    
+    
     // Add sounds
     flapSnd = game.add.audio('flap');
     scoreSnd = game.add.audio('score');
@@ -186,9 +297,11 @@ function reset() {
     gameOver = false;
     score = 0;
     credits.renderable = true;
-    scoreText.setText("DON'T\nTOUCH\nMY\nBIRDIE");
+    scoreText.setText("SNAPPY\nBIRD");
     instText.setText("TOUCH TO FLAP\nBIRDIE WINGS");
     gameOverText.renderable = false;
+    postScoreText.renderable = false;
+    kikThisText.renderable = false;
     birdie.body.allowGravity = false;
     birdie.angle = 0;
     birdie.reset(game.world.width / 4, game.world.height / 2);
@@ -220,6 +333,15 @@ function flap() {
     if (!gameOver) {
         birdie.body.velocity.y = -FLAP;
         flapSnd.play();
+    } else {
+        // Check if the touch event is within our text for posting a score
+        if (postScoreClickArea && Phaser.Rectangle.contains(postScoreClickArea, game.input.x, game.input.y)) {
+            postScore();
+        }
+        // Check if the touch event is within our text for sending a kik message
+        else if (Clay.Environment.platform == 'kik' && kikThisClickArea && Phaser.Rectangle.contains(kikThisClickArea, game.input.x, game.input.y)) {
+            kikThis();
+        }
     }
 }
 
@@ -301,8 +423,15 @@ function setGameOver() {
     hiscore = hiscore ? hiscore : score;
     hiscore = score > parseInt(hiscore, 10) ? score : hiscore;
     window.localStorage.setItem('hiscore', hiscore);
-    gameOverText.setText("GAMEOVER\n\nHISCORE\n" + hiscore);
+    // removed GAMEOVER\n\n
+    gameOverText.setText("HISCORE\n" + hiscore);
     gameOverText.renderable = true;
+    
+    postScoreText.renderable = true;
+    if (Clay.Environment.platform == 'kik') {
+        kikThisText.renderable = true;
+    }
+    
     // Stop all fingers
     fingers.forEachAlive(function(finger) {
         finger.body.velocity.x = 0;
@@ -369,14 +498,14 @@ function update() {
     if (!gameStarted || gameOver) {
         // Shake instructions text
         instText.scale.setTo(
-            2 + 0.1 * Math.sin(game.time.now / 100),
-            2 + 0.1 * Math.cos(game.time.now / 100)
+            1 + 0.1 * Math.sin(game.time.now / 100),
+            1 + 0.1 * Math.cos(game.time.now / 100)
         );
     }
     // Shake score text
     scoreText.scale.setTo(
-        2 + 0.1 * Math.cos(game.time.now / 100),
-        2 + 0.1 * Math.sin(game.time.now / 100)
+        1 + 0.1 * Math.cos(game.time.now / 100),
+        1 + 0.1 * Math.sin(game.time.now / 100)
     );
     // Update clouds timer
     cloudsTimer.update();
